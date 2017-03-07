a825bd7307a5c5d3b3378ca32e1b0707b7dca7bb
Nikita Romanenko <nikita241296@gmail.com>
2017-03-07 22:20:04 +0200

    gitignore update

---
+++ app/src/main/java/com/nanddgroup/itea_project/MainActivity.java
@@ -0,0 +1,12 @@
+package com.nanddgroup.itea_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
